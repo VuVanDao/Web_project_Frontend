6ca4c13a9cea1c2f17e5fe8caa5540a1006cb133
Van Dao <dao28901@gmail.com>
2025-01-16 20:27:29 +0700

    #79 Upsert Thông Tin Bác Sĩ vào Table Doctor Infor

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -10,6 +10,7 @@ import Select from "react-select";
 import LoadingData from "./LoadingData";
 import { LANGUAGES } from "../../../utils/constant";
 import userService from "../../../services/userService";
+import { toast } from "react-toastify";
 const mdParser = new MarkdownIt();
 
 class ManageDoctor extends Component {
@@ -25,16 +26,17 @@ class ManageDoctor extends Component {
       paymentArr: [],
       provinceArr: [],
       isLoading: true,
-      selectedOptionDisplay: "",
       selectedPayment: "",
       selectedPrice: "",
       selectedProvince: "",
+      clinic: "",
+      addressClinic: "",
+      note: "",
     };
   }
   handleChange = async (selectedOption) => {
     this.setState({
       selectedOption: selectedOption.value,
-      selectedOptionDisplay: selectedOption,
     });
     let res = await userService.getDetailDoctor(selectedOption.value);
     if (
@@ -55,19 +57,11 @@ class ManageDoctor extends Component {
       });
     }
   };
-  handleChangePayment = (selectedPayment) => {
+  handleChangeSelect = (selected, id) => {
+    let stateClone = { ...this.state };
+    stateClone[id.name] = selected.value;
     this.setState({
-      selectedPayment: selectedPayment.value,
-    });
-  };
-  handleChangePrice = (selectedPrice) => {
-    this.setState({
-      selectedPrice: selectedPrice.value,
-    });
-  };
-  handleChangeProvince = (selectedProvince) => {
-    this.setState({
-      selectedProvince: selectedProvince.value,
+      ...stateClone,
     });
   };
   buildInputSelect = (data, id) => {
@@ -90,8 +84,10 @@ class ManageDoctor extends Component {
           data.map((item, index) => {
             let object = {};
             object.label =
-              language === LANGUAGES.VI ? item.valueVI : item.valueEN;
-            object.value = item.id;
+              language === LANGUAGES.VI
+                ? item.valueVI + " VND"
+                : item.valueEN + " USD";
+            object.value = item.keyMap;
             result.push(object);
           });
           break;
@@ -100,7 +96,7 @@ class ManageDoctor extends Component {
             let object = {};
             object.label =
               language === LANGUAGES.VI ? item.valueVI : item.valueEN;
-            object.value = item.id;
+            object.value = item.keyMap;
             result.push(object);
           });
           break;
@@ -109,7 +105,7 @@ class ManageDoctor extends Component {
             let object = {};
             object.label =
               language === LANGUAGES.VI ? item.valueVI : item.valueEN;
-            object.value = item.id;
+            object.value = item.keyMap;
             result.push(object);
           });
           break;
@@ -183,31 +179,82 @@ class ManageDoctor extends Component {
       });
     }
   }
-  handleSaveMarkdown = () => {
-    // this.props.saveInfoDoctor({
-    //   textMarkdown: this.state.textMarkdown,
-    //   htmlMarkdown: this.state.htmlMarkdown,
-    //   introduce: this.state.introduce,
-    //   id: this.state.selectedOption,
-    // });
-    // this.setState({
-    //   textMarkdown: "",
-    //   htmlMarkdown: "",
-    //   introduce: "",
-    //   selectedOption: "",
-    //   selectedOptionDisplay: "",
-    // });
-    console.log(
-      ">>>",
-      this.state.selectedPayment,
-      this.state.selectedPrice,
-      this.state.selectedProvince
-    );
-  };
-  handleChangeTextArea = (event) => {
-    this.setState({
-      introduce: event.target.value,
+  Validate = () => {
+    let result = true;
+    let arrCheck = [
+      "introduce",
+      "selectedOption",
+      "selectedPayment",
+      "selectedPrice",
+      "selectedProvince",
+      "clinic",
+      "addressClinic",
+      "note",
+    ];
+    arrCheck.map((item) => {
+      if (!this.state[item]) result = false;
     });
+    return result;
+  };
+  handleSaveMarkdown = () => {
+    console.log("<<>><<>>", this.state);
+
+    let result = this.Validate();
+    if (!result) {
+      toast.error("Not complete information");
+    } else {
+      this.props.saveInfoDoctor({
+        textMarkdown: this.state.textMarkdown,
+        htmlMarkdown: this.state.htmlMarkdown,
+        introduce: this.state.introduce,
+        id: this.state.selectedOption,
+        paymentId: this.state.selectedPayment,
+        provinceId: this.state.selectedProvince,
+        priceId: this.state.selectedPrice,
+        nameClinic: this.state.clinic,
+        addressClinic: this.state.addressClinic,
+        note: this.state.note,
+      });
+      this.setState({
+        textMarkdown: "",
+        htmlMarkdown: "",
+        introduce: "",
+        selectedOption: "",
+        selectedPayment: "",
+        selectedPrice: "",
+        selectedProvince: "",
+        clinic: "",
+        addressClinic: "",
+        note: "",
+      });
+    }
+  };
+  handleChangeText = (event, id) => {
+    switch (id) {
+      case "text-area":
+        this.setState({
+          introduce: event.target.value,
+        });
+        break;
+      case "clinic":
+        this.setState({
+          clinic: event.target.value,
+        });
+        break;
+      case "addressClinic":
+        this.setState({
+          addressClinic: event.target.value,
+        });
+        break;
+      case "note":
+        this.setState({
+          note: event.target.value,
+        });
+        break;
+
+      default:
+        break;
+    }
   };
   /* life cycle
     Run component
@@ -218,16 +265,9 @@ class ManageDoctor extends Component {
   render() {
     // console.log(">>>", this.props.priceArr);
     // console.log(">>>state", this.state.priceArr);
-    let {
-      options,
-      priceArr,
-      paymentArr,
-      provinceArr,
-      selectedPayment,
-      selectedPrice,
-      selectedProvince,
-    } = this.state;
+    let { options, priceArr, paymentArr, provinceArr } = this.state;
     let { language } = this.props;
+
     return (
       <>
         {this.state.isLoading ? (
@@ -244,7 +284,6 @@ class ManageDoctor extends Component {
                     <FormattedMessage id="menu.system.doctor.ChooseDoctor" />
                   </label>
                   <Select
-                    value={this.state.selectedOptionDisplay}
                     onChange={this.handleChange}
                     options={options}
                     placeholder={
@@ -261,7 +300,10 @@ class ManageDoctor extends Component {
                     className="form-control"
                     style={{ height: "100px" }}
                     value={this.state.introduce}
-                    onChange={(event) => this.handleChangeTextArea(event)}
+                    onChange={(event) =>
+                      this.handleChangeText(event, "text-area")
+                    }
+                    name="text-area"
                   ></textarea>
                 </div>
                 <div className="row mt-4">
@@ -272,7 +314,8 @@ class ManageDoctor extends Component {
                     <Select
                       id="payment"
                       options={paymentArr}
-                      onChange={this.handleChangePayment}
+                      onChange={this.handleChangeSelect}
+                      name="selectedPayment"
                       placeholder={
                         <FormattedMessage id="menu.system.doctor.payment" />
                       }
@@ -285,7 +328,8 @@ class ManageDoctor extends Component {
                     <Select
                       id="price"
                       options={priceArr}
-                      onChange={this.handleChangePrice}
+                      onChange={this.handleChangeSelect}
+                      name="selectedPrice"
                       placeholder={
                         <FormattedMessage id="menu.system.doctor.price" />
                       }
@@ -298,7 +342,8 @@ class ManageDoctor extends Component {
                     <Select
                       id="province"
                       options={provinceArr}
-                      onChange={this.handleChangeProvince}
+                      onChange={this.handleChangeSelect}
+                      name="selectedProvince"
                       placeholder={
                         <FormattedMessage id="menu.system.doctor.province" />
                       }
@@ -310,19 +355,38 @@ class ManageDoctor extends Component {
                     <label htmlFor="clinic">
                       <FormattedMessage id="menu.system.doctor.clinic" />
                     </label>
-                    <input id="clinic" className="form-control" />
+                    <input
+                      id="clinic"
+                      className="form-control"
+                      name="clinic"
+                      onChange={(event) =>
+                        this.handleChangeText(event, "clinic")
+                      }
+                    />
                   </div>
                   <div className="col-md-4 ">
                     <label htmlFor="addressClinic">
                       <FormattedMessage id="menu.system.doctor.addressClinic" />
                     </label>
-                    <input id="addressClinic" className="form-control" />
+                    <input
+                      id="addressClinic"
+                      className="form-control"
+                      name="addressClinic"
+                      onChange={(event) =>
+                        this.handleChangeText(event, "addressClinic")
+                      }
+                    />
                   </div>
                   <div className="col-md-4 ">
                     <label htmlFor="note">
                       <FormattedMessage id="menu.system.doctor.note" />
                     </label>
-                    <input id="note" className="form-control" />
+                    <input
+                      id="note"
+                      className="form-control"
+                      name="note"
+                      onChange={(event) => this.handleChangeText(event, "note")}
+                    />
                   </div>
                 </div>
               </div>
