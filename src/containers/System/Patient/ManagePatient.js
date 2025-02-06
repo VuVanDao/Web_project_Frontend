3bd7a3ddb9c08c9d20076026f5085c62a6c6919a
Van Dao <dao28901@gmail.com>
2025-02-06 17:59:09 +0700

    sửa lại cái build Schedule

--- src/containers/System/Patient/ManagePatient.js
+++ src/containers/System/Patient/ManagePatient.js
@@ -8,6 +8,7 @@ import * as actions from "../../../store/actions";
 import { LANGUAGES, dateFormat } from "../../../utils/constant";
 import DatePicker from "../../../components/Input/DatePicker";
 import { toast } from "react-toastify";
+import moment from "moment";
 import _ from "lodash";
 import userService from "../../../services/userService";
 class ManagePatient extends Component {
@@ -15,69 +16,66 @@ class ManagePatient extends Component {
     super(props);
     this.state = {
       arrDoctor: [],
-      selectedDoctor: "",
       currentDate: "",
       PatientBookedList: [],
+      options: [],
     };
   }
-  buildInputSelect = (data) => {
-    let result = [];
+  setLanguageSchedule = () => {
+    let arrDay = [];
     let { language } = this.props;
-    if (data && data.length > 0) {
-      data.map((item, index) => {
-        let object = {};
-        let labelVi = item.lastName + " " + item.firstName;
-        let labelEn = item.firstName + " " + item.lastName;
-        object.label = language === LANGUAGES.VI ? labelVi : labelEn;
-        object.value = item.id;
-        result.push(object);
-      });
+    for (let i = 0; i < 7; i++) {
+      let object = {};
+      if (language === LANGUAGES.VI) {
+        object.label = moment(new Date()).add(i, "days").format("dddd - DD/MM");
+      } else {
+        object.label = moment(new Date())
+          .add(i, "days")
+          .locale("en")
+          .format("dddd - DD/MM");
+      }
+      object.value = moment(new Date()).add(i, "days").startOf("day").valueOf();
+      arrDay.push(object);
     }
-    return result;
-  };
-  handleOnChangeDataPicker = async (value) => {
     this.setState({
-      currentDate: value[0],
+      options: arrDay,
     });
-    let formattedDate = new Date(this.state.currentDate).getTime().toString();
+    return arrDay;
+  };
+  handlePickSchedule = async (event) => {
     let res = await userService.PatientBooked(
-      this.state.selectedDoctor,
-      formattedDate
+      this.props.userInfo.id,
+      event.value
     );
-    console.log(">><<", res);
     if (res && res.errCode === 0) {
       this.setState({
         PatientBookedList: res.data,
       });
     }
   };
-  handleChange = async (selectedDoctor) => {
-    this.setState({
-      selectedDoctor: selectedDoctor.value,
-    });
-  };
-  componentDidMount() {
-    this.props.getAllListDoctor();
-    this.props.fetchAllSchedule();
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.AllDoctor !== this.props.AllDoctor) {
-      let result = this.buildInputSelect(this.props.AllDoctor);
+
+  async componentDidMount() {
+    let arrDay = this.setLanguageSchedule();
+    let res = await userService.PatientBooked(
+      this.props.userInfo.id,
+      arrDay[0].value
+    );
+    if (res && res.errCode === 0) {
       this.setState({
-        arrDoctor: result,
+        PatientBookedList: res.data ? res.data : [],
       });
     }
+  }
+  componentDidUpdate(prevProps, prevState) {
     if (prevProps.language !== this.props.language) {
-      let result = this.buildInputSelect(this.props.AllDoctor);
-      this.setState({
-        arrDoctor: result,
-      });
+      this.setLanguageSchedule();
     }
   }
 
   render() {
-    let { arrDoctor, PatientBookedList } = this.state;
-    let { language } = this.props;
+    let { options, PatientBookedList } = this.state;
+    let { language, userInfo } = this.props;
+    // console.log("<><><>", userInfo);
 
     return (
       <>
@@ -87,32 +85,29 @@ class ManagePatient extends Component {
               <FormattedMessage id={"Patient.title"} />
             </div>
             <div className="row mt-5">
-              <div className="col-6">
-                <label>
-                  <FormattedMessage id={"Patient.ChooseDoctor"} />
-                </label>
-                <Select onChange={this.handleChange} options={arrDoctor} />
+              <div className="col-12 mb-3">
+                {language === LANGUAGES.VI
+                  ? "Bác sĩ: " + userInfo.lastName + " " + userInfo.firstName
+                  : "Doctor: " + userInfo.firstName + " " + userInfo.lastName}
               </div>
               <div className="col-6">
                 <label>
                   <FormattedMessage id={"Patient.ChooseSchedule"} />
                 </label>
-                <DatePicker
-                  onChange={(event) => this.handleOnChangeDataPicker(event)}
-                  className="form-control"
-                  options={{ minDate: new Date() }}
-                />
+                <Select onChange={this.handlePickSchedule} options={options} />
               </div>
             </div>
             <div className="container mt-5">
-              <table class="table table-hover">
+              <table className="table table-hover">
                 <thead>
                   <tr>
+                    <th scope="col">Time</th>
                     <th scope="col">Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Address</th>
                     <th scope="col">Gender</th>
                     <th scope="col">PhoneNumber</th>
+                    <th scope="col">Actions</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -120,12 +115,35 @@ class ManagePatient extends Component {
                     PatientBookedList.length > 0 &&
                     PatientBookedList.map((item, index) => {
                       return (
-                        <tr>
+                        <tr key={`item-${index}`}>
+                          <td>
+                            {language === LANGUAGES.VI
+                              ? item.timeData.valueVI
+                              : item.timeData.valueEN}
+                          </td>
                           <td>{item.bookingData.firstName}</td>
                           <td>{item.bookingData.email}</td>
                           <td>{item.bookingData.address}</td>
                           <td>{item.bookingData.gender}</td>
                           <td>{item.bookingData.phoneNumber}</td>
+                          <td>
+                            <div>
+                              <button
+                                className="btn-update"
+                                // onClick={() => this.ShowModalUpdateUser(item.id)}
+                              >
+                                <FormattedMessage id="system.user-manage.edit-user" />
+                                <i className="fas fa-user-edit"></i>
+                              </button>
+                              <button
+                                className="btn-Delete"
+                                // onClick={() => this.ShowModalDeleteUser(item.id)}
+                              >
+                                <FormattedMessage id="system.user-manage.del-user" />
+                                <i className="fas fa-user-times"></i>
+                              </button>
+                            </div>
+                          </td>
                         </tr>
                       );
                     })}
@@ -144,6 +162,7 @@ const mapStateToProps = (state) => {
     language: state.app.language,
     AllDoctor: state.admin.AllDoctor,
     AllSchedule: state.admin.AllSchedule,
+    userInfo: state.user.userInfo,
   };
 };
 
